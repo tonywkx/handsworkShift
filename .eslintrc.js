7cde4d838e8fa9e879a20a047f75cae521f0177b
Tony Kolesnikov <tonyk@MacBook-Air-Tony.local>
2025-10-16 22:13:52 +0400

    init: start project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,112 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
-};
+
+  parser: '@typescript-eslint/parser',
+
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+
+  plugins: [
+    '@typescript-eslint',
+    'import',
+    'prettier',
+    'simple-import-sort',
+    'promise',
+  ],
+
+  extends: [
+    '@react-native-community',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+
+  rules: {
+    quotes: [
+      'error',
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true,
+      },
+    ],
+    'react/jsx-no-literals': 'off',
+    'padding-line-between-statements': [
+      'error',
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'return',
+      },
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'case',
+      },
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'default',
+      },
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'block-like',
+      },
+    ],
+    'prefer-const': ['error'],
+    'simple-import-sort/imports': [
+      'warn',
+      {
+        groups: [['^\\u0000'], ['^react', '^[^.]'], ['^src/'], ['^\\.']],
+      },
+    ],
+
+    'max-lines': ['error', 500],
+    'no-console': 'error',
+    'object-shorthand': 'error',
+    'no-unneeded-ternary': 'error',
+    'no-nested-ternary': 'error',
+    'newline-before-return': 'warn',
+    semi: ['error', 'never'],
+
+    // prettier
+    'prettier/prettier': [
+      'error',
+      {
+        tabWidth: 2,
+        trailingComma: 'all',
+        semi: false,
+        arrowParens: 'always',
+        singleQuote: true,
+        printWidth: 80,
+        bracketSpacing: true,
+      },
+    ],
+
+    'import/newline-after-import': 'error',
+
+    // react
+    'react/react-in-jsx-scope': 'off',
+
+    'no-shadow': 'off',
+
+    'react-native/no-unused-styles': 'error',
+    'react-native/split-platform-components': 'warn',
+    'react-native/no-color-literals': 'warn',
+    'react-native/no-single-element-style-arrays': 'error',
+    'react/sort-comp': 'warn',
+
+    // promise
+    'promise/prefer-await-to-then': 'warn',
+    'promise/prefer-await-to-callbacks': 'warn',
+
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      { fixStyle: 'inline-type-imports' },
+    ],
+  },
+}
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,12 @@
 module.exports = {
-  arrowParens: 'avoid',
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
   singleQuote: true,
+  semi: false,
   trailingComma: 'all',
+  tabWidth: 2,
+  printWidth: 80,
+  semi: false,
+  arrowParens: 'always',
+  endOfLine: 'auto',
 };
